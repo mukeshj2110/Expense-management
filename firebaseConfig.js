247411b1c804aa4e21d1aba34273ffad2f1bb1df
mukeshj2110 <mukeshjadhav680@gmail.com>
2021-05-20 16:20:37 +0530

    added backend firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyD9ZpJPnyK4neNdc8wCI9VWsaV5MRYx5hQ",
+      authDomain: "expense-management-ced26.firebaseapp.com",
+      projectId: "expense-management-ced26",
+      storageBucket: "expense-management-ced26.appspot.com",
+      messagingSenderId: "307113238195",
+      appId: "1:307113238195:web:4a5d0bf7556bb6c3215ac6"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    const db = firebase.firestore();
+
+  
---
+++ index.js
@@ -0,0 +1,82 @@
+const inputElement = document.querySelector("#inputAmount");
+const element = document.querySelector("#btnAddExpense");
+const signinEl = document.querySelector("#signinLink");
+const headingEl = document.querySelector("#headingTotal");
+const inputDescEl = document.querySelector("#inputDesc");
+const expenseTableEl = document.querySelector("#expenseTable");
+const alertEl = document.querySelector("#alert");
+
+let totalExpense = "Expense: " + 0;
+
+
+
+function addExpenseToTotal() {
+  const expenseItem = {};
+  const textAmount = inputElement.value;
+  const textDesc = inputDescEl.value;
+
+  const expense = parseInt(textAmount, 10);
+
+  expenseItem.desc = textDesc;
+  expenseItem.amount = expense;
+  expenseItem.moment = new Date();
+  if (!isNaN(expense)) {
+    addExpenseOnFirestore(textDesc, expense);
+    inputElement.value = "";
+    inputDescEl.value = "";
+  }
+
+  totalExpense = totalExpense + expense;
+  const someText = `Total: ${totalExpense}`;
+
+  headingEl.textContent = someText;
+
+}
+
+
+function getDateString(momento) {
+  return momento.toDate().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
+function deleteItem(docId, amount) {
+  deleteFromFirebase(docId, amount);
+}
+
+function renderList(arrOfList) {
+  
+  const allExpenseHTML = arrOfList.map((expense) => createListItem(expense));
+  const joinedAllExpenseHTML = allExpenseHTML.join("");
+  expenseTableEl.innerHTML = joinedAllExpenseHTML;
+}
+
+element.addEventListener("click", addExpenseToTotal, false);
+getDocuments();
+
+// View layer
+function createListItem({ desc, amount, createdAt, docId }) {
+
+  return `
+      <li class="list-group-item d-flex justify-content-between">
+      <div class="d-flex flex-column">
+      ${desc}
+          <small class="text-muted">${getDateString(createdAt)}</small>
+      </div>
+      <div>
+          <span class="px-5">
+          ${amount}
+          </span>
+          <button 
+            type="button" 
+            class="btn btn-outline-danger btn-sm"
+            onClick="deleteItem('${docId}', ${amount})"
+          >
+            <i class="fas fa-trash"></i>
+          </button>
+      </div>
+    </li>
+      `;
+}
